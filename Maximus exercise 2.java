35828275db90195a394ec1e5868298a59c2fc74a
Maximus Mackert <46829967+TrashMan7@users.noreply.github.com>
2019-01-31 17:52:29 -0700

    This is my work so far
    simple equations

---
+++ Maximus exercise 2.java
@@ -0,0 +1,10 @@
+/* Maximus Mackert
+1/30/19This program is two math expressions that display the area and preimiter of a rectangle */
+class Area {
+	public static void main(String[] args) {
+		System.out.println("4.5 * 7.9");
+		System.out.println(4.5 * 7.9);
+		System.out.println("(4.5 * 2) + (7.9 * 2)");
+		System.out.println((4.5 * 2) + (7.9 * 2));
+	}
+}
